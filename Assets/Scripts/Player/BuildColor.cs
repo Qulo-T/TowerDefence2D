12c884ed463243aa03199e622b3c44f7b60d5604
QuloZ <36335404+QuloZ@users.noreply.github.com>
2020-02-03 18:00:35 +0300

    AddArt
    backgroud

---
+++ Assets/Scripts/Player/BuildColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildColor : MonoBehaviour
+{
+    [SerializeField] Color color1;
+    [SerializeField] Color color2;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        spriteRenderer.color = color2;
+    }
+
+    private void OnMouseExit()
+    {
+        spriteRenderer.color = color1;
+    }
+}
