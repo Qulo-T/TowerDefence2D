5a6f7143cd5b61fffcc06ae3577c31613e486a8d
QuloZ <36335404+QuloZ@users.noreply.github.com>
2020-02-26 22:02:09 +0300

    Test Raycast

---
+++ Assets/Scripts/Player/TestRaycast.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRaycast : MonoBehaviour
+{
+    public float radius;
+    int layerMask = 1 << 8;
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.up * radius, Color.green,1,false);
+    }
+    private void RayTest()
+    {
+        
+        RaycastHit2D[] hit = Physics2D.CircleCastAll(transform.position, radius, Vector2.zero, radius, layerMask); 
+
+        foreach (RaycastHit2D cast in hit)
+        {
+            if (cast.collider != null)
+            {
+               Debug.Log( cast.collider.gameObject.name);
+            }
+        }
+    }
+    private void OnMouseDown()
+    {
+        RayTest();
+    }
+}
